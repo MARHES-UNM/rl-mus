59946fb25e514d397900d4e64ac2be9ddeb5ec88
Jean-Elie Pierre <gno320@gmail.com>
2023-04-28 11:04:36 -0400

    refactored gui, entities; created unittest for uavs

--- tests/test_uav_sim.py
+++ tests/test_uav_sim.py
@@ -3,7 +3,7 @@ import unittest
 
 import context
 
-from uav_sim.envs.uav_sim import UavSim, Quadrotor
+from uav_sim.envs.uav_sim import UavSim
 
 
 class TestUavSim(unittest.TestCase):
@@ -19,18 +19,6 @@ class TestUavSim(unittest.TestCase):
             self.env.step(actions)
             self.env.render()
             t += 1
-
-
-class TestUav(unittest.TestCase):
-    def setUp(self):
-        self.uav = Quadrotor(3, 4, 5)
-
-    def test_rotation_matrix(self):
-        for i in range(9):
-            self.uav._state[6:9] = np.random.rand(3) * (np.pi + np.pi) - np.pi
-            rot_mat = self.uav.rotation_matrix()
-            np.testing.assert_almost_equal(np.linalg.det(rot_mat), 1.0)
-
 
 if __name__ == "__main__":
     unittest.main()
