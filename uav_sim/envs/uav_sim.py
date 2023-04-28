e9c7e9e46e920004a3ffe7e67881671a930c2bf6
Jean-Elie Pierre <gno320@gmail.com>
2023-04-28 12:04:30 -0400

    able to plot multi uavs

--- uav_sim/envs/uav_sim.py
+++ uav_sim/envs/uav_sim.py
@@ -16,6 +16,7 @@ class UavSim:
         self._seed = env_config.get("seed", None)
         self.render_mode = env_config.get("render_mode", "human")
         self.num_uavs = env_config.get("num_uavs", 4)
+        # self.env_max_x = env_config.get("max_x", )
         self.fig = None
         self.clock = None
         self.gui = None
@@ -28,10 +29,11 @@ class UavSim:
         return self._time_elapsed
 
     def step(self, actions):
-        action = np.random.rand(4) * (self.uav.max_f - self.uav.min_f) + self.uav.min_f
-        action = np.ones(4) * self.uav.m * self.uav.g / 4
+        for uav in self.uavs:
+            action = np.random.rand(4) * (uav.max_f - uav.min_f) + uav.min_f
+            action = np.ones(4) * uav.m * uav.g / 4
 
-        self.uav.step(action)
+            uav.step(action)
 
         self._time_elapsed += self.dt
 
@@ -43,13 +45,38 @@ class UavSim:
         return [seed]
 
     def reset(self, seed=None, options=None):
-        self.seed(seed)
+        """_summary_
 
-        self.uav = Quadrotor(2, 4, 5)
+        Args:
+            seed (_type_, optional): _description_. Defaults to None.
+            options (_type_, optional): _description_. Defaults to None.
+        """
+        if self.gui is not None:
+            self.close_gui()
+
+        self.seed(seed)
+        self.uavs = []
+
+        uav_pos = [[2, 4, 5], [1, 1, 1], [1, 1, 2], [2, 1, 3]]
+        for pos in uav_pos:
+            uav = Quadrotor(*pos)
+            self.uavs.append(uav)
+        # for idx in range(self.num_uavs):
+        #     # x = np.random.rand()
+        #     # y =
+        #     # z =
+        #     self.uavs.append(uav)]
+
+        # self.uav = Quadrotor(2, 4, 5)
 
     def render(self, mode="human"):
         if self.render_mode == "human":
             if self.gui is None:
-                self.gui = Gui(self.uav)
+                self.gui = Gui(self.uavs)
             else:
                 self.gui.update(self.time_elapsed)
+
+    def close_gui(self):
+        if self.gui is None:
+            self.gui.close()
+        self.gui = None
