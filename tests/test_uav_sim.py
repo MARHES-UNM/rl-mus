ac869e51fe82e58cfef446c9af266e37e38f66df
Jean-Elie Pierre <gno320@gmail.com>
2023-04-29 02:00:35 -0400

    tesing env

--- tests/test_uav_sim.py
+++ tests/test_uav_sim.py
@@ -9,7 +9,7 @@ from uav_sim.envs.uav_sim import UavSim
 
 class TestUavSim(unittest.TestCase):
     def setUp(self):
-        self.env = UavSim()
+        self.env = UavSim(env_config={"dt":0.1})
 
     # def test_lqr_controller(self):
     #     positions = np.array([[0.5, 0.5, 1], [0.5, 2, 2], [2, 0.5, 2], [2, 2, 1]])
@@ -37,39 +37,45 @@ class TestUavSim(unittest.TestCase):
     #         self.env.step(actions)
     #         self.env.render()
 
-        # K = self.env.uavs[0].calc_k()
-        # actions = {}
+    # K = self.env.uavs[0].calc_k()
+    # actions = {}
 
-        # for i in range(100):
-        #     for idx, pos in enumerate(des_pos):
-        #         cur_pos = self.env.uavs[idx].state
-        #         actions[idx] = np.dot(-K, pos - cur_pos)
-        #         # actions[i] = np.dot(K, )
-        #     self.env.step(actions)
-        #     self.env.render()
+    # for i in range(100):
+    #     for idx, pos in enumerate(des_pos):
+    #         cur_pos = self.env.uavs[idx].state
+    #         actions[idx] = np.dot(-K, pos - cur_pos)
+    #         # actions[i] = np.dot(K, )
+    #     self.env.step(actions)
+    #     self.env.render()
 
-    # def test_setting_uav_pos(self):
-    #     uav_pos = [[0.5, 0.5, 1], [0.5, 2, 2], [2, 0.5, 2], [2, 2, 1]]
-    #     for idx, pos in enumerate(uav_pos):
-    #         self.env.uavs[idx]._state[0] = pos[0]
-    #         self.env.uavs[idx]._state[1] = pos[1]
-    #         self.env.uavs[idx]._state[2] = pos[2]
+    def test_setting_uav_pos(self):
+        self.env = UavSim(env_config={"dt":0.1})
+        uav_pos = np.array([[0.5, 0.5, 1], [0.5, 2, 2], [2, 0.5, 2], [2, 2, 1]])
+        for idx, pos in enumerate(uav_pos):
+            self.env.uavs[idx]._state[0] = pos[0]
+            self.env.uavs[idx]._state[1] = pos[1]
+            self.env.uavs[idx]._state[2] = pos[2]
 
-    #     for i in range(20):
-    #         actions = {_id: np.zeros(4) for _id in range(self.env.num_uavs)}
-    #         self.env.step(actions)
-    #         self.env.render()
+        for i in range(20):
+            actions = {_id: np.zeros(4) for _id in range(self.env.num_uavs)}
+            self.env.step(actions)
 
-    # def test_render(self):
-    #     tf = 100
-    #     t = 0
-    #     actions = {}
-    #     while t < tf:
-    #         for i in range(self.env.num_uavs):
-    #             actions[i] = np.ones(4) * self.env.uavs[i].m * self.env.uavs[i].g / 4
-    #         self.env.step(actions)
-    #         self.env.render()
-    #         t += 1
+        for idx in range(self.env.num_uavs):
+            np.testing.assert_array_almost_equal(
+                uav_pos[idx, 0:2], self.env.uavs[idx].state[0:2]
+            )
+            np.testing.assert_almost_equal(0.0, self.env.uavs[idx].state[2])
+
+    def test_render(self):
+        tf = 100
+        t = 0
+        actions = {}
+        while t < tf:
+            for i in range(self.env.num_uavs):
+                actions[i] = np.ones(4) * self.env.uavs[i].m * self.env.uavs[i].g / 4
+            self.env.step(actions)
+            self.env.render()
+            t += 1
 
 
 if __name__ == "__main__":
