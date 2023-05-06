2e0c183c918fe7be7fde5f93fdcf72e1a1dd9786
Jean-Elie Pierre <gno320@gmail.com>
2023-05-06 07:54:42 -0400

    plotting trajectory

--- uav_sim/utils/gui.py
+++ uav_sim/utils/gui.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import sys
 import matplotlib
 
@@ -17,20 +18,94 @@ class TargetSprite:
 
 
 class UavSprite:
-    def __init__(self, ax):
+    def __init__(self, ax, uav=None):
         self.ax = ax
-        (self.l1,) = self.ax.plot(
+        self.uav = uav
+
+        (self.l1,) = self.ax["ax_3d"].plot(
             [], [], [], color="blue", linewidth=1, antialiased=False
         )
-        (self.l2,) = self.ax.plot(
+        (self.l2,) = self.ax["ax_3d"].plot(
             [], [], [], color="blue", linewidth=1, antialiased=False
         )
 
-        (self.cm,) = self.ax.plot([], [], [], "k.")
-        (self.rotor1,) = self.ax.plot([], [], [], "k.")
-        (self.rotor2,) = self.ax.plot([], [], [], "k.")
-        (self.rotor3,) = self.ax.plot([], [], [], "k.")
-        (self.rotor4,) = self.ax.plot([], [], [], "k.")
+        (self.cm,) = self.ax["ax_3d"].plot([], [], [], "k.")
+
+        # TODO: plot rotors
+        # (self.rotor1,) = self.ax["ax_3d"].plot([], [], [], "k.")
+        # (self.rotor2,) = self.ax["ax_3d"].plot([], [], [], "k.")
+        # (self.rotor3,) = self.ax["ax_3d"].plot([], [], [], "k.")
+        # (self.rotor4,) = self.ax["ax_3d"].plot([], [], [], "k.")
+
+        self.trajectory = {
+            "t": [],
+            "x": [],
+            "y": [],
+            "z": [],
+            "psi": [],
+        }
+
+        (self.x_bar,) = self.ax["ax_error_x"].plot([], [], label=f"id: {self.uav.id}")
+        (self.y_bar,) = self.ax["ax_error_y"].plot([], [], label=f"id: {self.uav.id}")
+        (self.z_bar,) = self.ax["ax_error_z"].plot([], [], label=f"id: {self.uav.id}")
+        (self.psi_bar,) = self.ax["ax_error_psi"].plot(
+            [], [], label=f"id: {self.uav.id}"
+        )
+
+        l = self.uav.l
+        self.points = np.array(
+            [
+                [-l, 0, 0],
+                [l, 0, 0],
+                [0, -l, 0],
+                [0, l, 0],
+                [0, 0, 0],  # cm
+                [-l, 0, 0],
+                [l, 0, 0],
+                [0, -l, 0],
+                [0, l, 0],
+            ]
+        ).T
+
+        self.t_lim = 3
+
+    def update(self, t):
+        R = self.uav.rotation_matrix()
+
+        body = np.dot(R, self.points)
+        body[0, :] += self.uav._state[0]
+        body[1, :] += self.uav._state[1]
+        body[2, :] += self.uav._state[2]
+
+        self.l1.set_data(body[0, 0:2], body[1, 0:2])
+        self.l1.set_3d_properties(body[2, 0:2])
+        self.l2.set_data(body[0, 2:4], body[1, 2:4])
+        self.l2.set_3d_properties(body[2, 2:4])
+        self.cm.set_data(body[0, 4:5], body[1, 4:5])
+        self.cm.set_3d_properties(body[2, 4:5])
+
+        self.trajectory["t"].append(t)
+        self.trajectory["x"].append(self.uav._state[0])
+        self.trajectory["y"].append(self.uav._state[1])
+        self.trajectory["z"].append(self.uav._state[2])
+        self.trajectory["psi"].append(self.uav._state[8])
+
+        self.ax["ax_error_x"].set_xlim(
+            left=max(0, t - self.t_lim), right=t + self.t_lim
+        )
+        self.x_bar.set_data(self.trajectory["t"], self.trajectory["x"])
+        self.ax["ax_error_y"].set_xlim(
+            left=max(0, t - self.t_lim), right=t + self.t_lim
+        )
+        self.y_bar.set_data(self.trajectory["t"], self.trajectory["y"])
+        self.ax["ax_error_z"].set_xlim(
+            left=max(0, t - self.t_lim), right=t + self.t_lim
+        )
+        self.z_bar.set_data(self.trajectory["t"], self.trajectory["z"])
+        self.ax["ax_error_psi"].set_xlim(
+            left=max(0, t - self.t_lim), right=t + self.t_lim
+        )
+        self.psi_bar.set_data(self.trajectory["t"], self.trajectory["psi"])
 
 
 class Gui:
@@ -42,21 +117,28 @@ class Gui:
             self.fig = plt.figure(figsize=(12, 6))
             gs0 = self.fig.add_gridspec(1, 2)
             gs00 = gs0[0].subgridspec(1, 1)
-            gs01 = gs0[1].subgridspec(3, 1)
-            self.ax = self.fig.add_subplot(gs00[0], projection="3d")
-            self.ax_errors = []
-            for i in range(3):
-                self.ax_errors.append(self.fig.add_subplot(gs01[i]))
+            gs01 = gs0[1].subgridspec(4, 1)
+            self.ax = {}
+            self.ax["ax_3d"] = self.fig.add_subplot(gs00[0], projection="3d")
+            # self.ax = self.fig.add_subplot(gs00[0], projection="3d")
+            self.ax["ax_error_x"] = self.fig.add_subplot(gs01[0])
+            self.ax["ax_error_x"].set_ylim([0, max_x])
+            self.ax["ax_error_y"] = self.fig.add_subplot(gs01[1])
+            self.ax["ax_error_y"].set_ylim([0, max_y])
+            self.ax["ax_error_z"] = self.fig.add_subplot(gs01[2])
+            self.ax["ax_error_z"].set_ylim([0, max_z])
+            self.ax["ax_error_psi"] = self.fig.add_subplot(gs01[3])
+            self.ax["ax_error_psi"].set_ylim([-np.pi, np.pi])
 
-        self.ax.set_xlim3d([0, max_x])
-        self.ax.set_ylim3d([0, max_y])
-        self.ax.set_zlim3d([0, max_z])
+        self.ax["ax_3d"].set_xlim3d([0, max_x])
+        self.ax["ax_3d"].set_ylim3d([0, max_y])
+        self.ax["ax_3d"].set_zlim3d([0, max_z])
 
-        self.ax.set_xlabel("X (m)")
-        self.ax.set_ylabel("Y (m)")
-        self.ax.set_zlabel("Z (m)")
+        self.ax["ax_3d"].set_xlabel("X (m)")
+        self.ax["ax_3d"].set_ylabel("Y (m)")
+        self.ax["ax_3d"].set_zlabel("Z (m)")
 
-        self.ax.set_title("Multi-UAV Simulation")
+        self.ax["ax_3d"].set_title("Multi-UAV Simulation")
 
         # add axis
         n_points = 5
@@ -64,25 +146,25 @@ class Gui:
         y_axis = np.linspace(0, max_y, n_points)
         z_axis = np.linspace(0, max_z, n_points)
 
-        self.ax.plot([0, 0], [0, 0], [0, 0], "k+")
-        self.ax.plot(
+        self.ax["ax_3d"].plot([0, 0], [0, 0], [0, 0], "k+")
+        self.ax["ax_3d"].plot(
             x_axis, np.zeros(n_points), np.zeros(n_points), "r--", linewidth=0.5
         )
-        self.ax.plot(
+        self.ax["ax_3d"].plot(
             np.zeros(n_points), y_axis, np.zeros(n_points), "g--", linewidth=0.5
         )
-        self.ax.plot(
+        self.ax["ax_3d"].plot(
             np.zeros(n_points), np.zeros(n_points), z_axis, "b--", linewidth=0.5
         )
 
         # (0, 0) is bottom left, (1, 1) is top right
         # Placement 0, 0 would be the bottom left, 1, 1 would be the top right.
-        self.time_display = self.ax.text2D(
-            0.75, 0.95, "", color="red", transform=self.ax.transAxes
+        self.time_display = self.ax["ax_3d"].text2D(
+            0.75, 0.95, "", color="red", transform=self.ax["ax_3d"].transAxes
         )
 
-        self.state_display = self.ax.text2D(
-            0, 0.95, "", color="green", transform=self.ax.transAxes
+        self.state_display = self.ax["ax_3d"].text2D(
+            0, 0.95, "", color="green", transform=self.ax["ax_3d"].transAxes
         )
 
         self.init_entities()
@@ -91,25 +173,27 @@ class Gui:
 
         # # plt.show(False)
         # plt.draw()
-        
+
         # self.background = self.fig.canvas.copy_from_bbox(self.ax.bbox)
 
     def init_entities(self):
         self.sprites = []
 
-        self.target_sprites = TargetSprite(self.ax, len(self.uavs))
+        self.target_sprites = TargetSprite(self.ax["ax_3d"], len(self.uavs))
 
-        for idx in range(len(self.uavs)):
-            uav_sprite = UavSprite(self.ax)
+        for uav in self.uavs:
+            uav_sprite = UavSprite(self.ax, uav)
             self.sprites.append(uav_sprite)
+        # for idx in range(len(self.uavs)):
+        #     uav_sprite = UavSprite(self.ax)
+        #     self.sprites.append(uav_sprite)
 
     # TODO: update this to use blit
     # https://matplotlib.org/stable/api/animation_api.html
     # https://stackoverflow.com/questions/11874767/how-do-i-plot-in-real-time-in-a-while-loop-using-matplotlib
     def update(self, time_elapsed):
-
         # self.fig.canvas.restore_region(self.background)
-        
+
         self.time_display.set_text(f"Sim time = {time_elapsed:.2f} s")
 
         target_points = np.array(
@@ -128,70 +212,39 @@ class Gui:
                 f"phi: {uav_state[6]:.2f}, theta: {uav_state[7]:.2f}, psi: {uav_state[8]:.2f}"
             )
 
-            R = uav.rotation_matrix()
-            l = uav.l
+            uav_sprite.update(time_elapsed)
 
-            points = np.array(
-                [
-                    [-l, 0, 0],
-                    [l, 0, 0],
-                    [0, -l, 0],
-                    [0, l, 0],
-                    [0, 0, 0],  # cm
-                    [-l, 0, 0],
-                    [l, 0, 0],
-                    [0, -l, 0],
-                    [0, l, 0],
-                ]
-            ).T
-            points = np.dot(R, points)
-
-            points[0, :] += uav._state[0]
-            points[1, :] += uav._state[1]
-            points[2, :] += uav._state[2]
-
-            uav_sprite.l1.set_data(points[0, 0:2], points[1, 0:2])
-            uav_sprite.l1.set_3d_properties(points[2, 0:2])
-            uav_sprite.l2.set_data(points[0, 2:4], points[1, 2:4])
-            uav_sprite.l2.set_3d_properties(points[2, 2:4])
-            uav_sprite.cm.set_data(points[0, 4:5], points[1, 4:5])
-            uav_sprite.cm.set_3d_properties(points[2, 4:5])
-            # uav_sprite.rotor1.set_data(points[0, 5:6], points[1, 5:6])
-            # uav_sprite.rotor1.set_3d_properties(points[2, 5:6])
-            # uav_sprite.rotor2.set_data(points[0, 6:7], points[1, 6:7])
-            # uav_sprite.rotor2.set_3d_properties(points[2, 6:7])
-            # uav_sprite.rotor3.set_data(points[0, 5:6], points[0, 5:6])
-            # uav_sprite.rotor3.set_3d_properties(points[2, 5:6])
-            # uav_sprite.rotor1.set_data(points[0, 5:6], points[0, 5:6])
-            # uav_sprite.rotor1.set_3d_properties(points[2, 5:6])
-            # uav_sprite.rotor2.set_data(points[0, 4:5], points[0, 4:5])
-        
         #     self.ax.draw_artist(uav_sprite.cm)
         # self.fig.canvas.blit(self.ax.bbox)
+        for key, ax in self.ax.items():
+            if key == "ax_3d":
+                continue
+            ax.legend()
+
         plt.pause(0.0000000000001)
 
     def keypress_routine(self, event):
         sys.stdout.flush()
         if event.key == "x":
-            y = list(self.ax.get_ylim3d())
+            y = list(self.ax["ax_3d"].get_ylim3d())
             y[0] += 0.2
             y[1] += 0.2
-            self.ax.set_ylim3d(y)
+            self.ax["ax_3d"].set_ylim3d(y)
         elif event.key == "w":
-            y = list(self.ax.get_ylim3d())
+            y = list(self.ax["ax_3d"].get_ylim3d())
             y[0] -= 0.2
             y[1] -= 0.2
-            self.ax.set_ylim3d(y)
+            self.ax["ax_3d"].set_ylim3d(y)
         elif event.key == "d":
-            x = list(self.ax.get_xlim3d())
+            x = list(self.ax["ax_3d"].get_xlim3d())
             x[0] += 0.2
             x[1] += 0.2
-            self.ax.set_xlim3d(x)
+            self.ax["ax_3d"].set_xlim3d(x)
         elif event.key == "a":
-            x = list(self.ax.get_xlim3d())
+            x = list(self.ax["ax_3d"].get_xlim3d())
             x[0] -= 0.2
             x[1] -= 0.2
-            self.ax.set_xlim3d(x)
+            self.ax["ax_3d"].set_xlim3d(x)
 
         elif event.key == "escape":
             exit(0)
