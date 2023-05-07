2d67ea0fcfef47c6ab6f8c70fc91146d4aa721f6
Jean-Elie Pierre <gno320@gmail.com>
2023-05-06 21:10:38 -0400

    added target_test.py

---
+++ tests/test_target.py
@@ -0,0 +1,25 @@
+from matplotlib import pyplot as plt
+
+from uav_sim.agents.uav import Target
+import unittest
+import numpy as np
+from uav_sim.utils.gui import Gui
+
+
+class TestUav(unittest.TestCase):
+    def setUp(self):
+        self.target = Target(_id=0, x=1.5, y=1.5, psi=np.pi / 2)
+        self.gui = Gui(target=self.target)
+
+    def test_target_move(self):
+        t = 0
+        for i in range(100):
+            t = i * self.target.dt
+            action = np.array([1, 0])
+            self.target.step(action)
+            self.gui.update(t)
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
