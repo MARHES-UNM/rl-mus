cfbc634b089a57292e12ed73ce85264c8019f361
Jean-Elie Pierre <gno320@gmail.com>
2023-10-02 22:29:43 -0400

    training with multiple values for

--- train_safety_layer.py
+++ train_safety_layer.py
@@ -91,11 +91,11 @@ def train(args):
     args.config["safety_layer_cfg"]["lr"] = tune.grid_search([5e-4])
     args.config["safety_layer_cfg"]["eps"] = tune.grid_search([0.05])
     args.config["safety_layer_cfg"]["eps_deriv"] = tune.grid_search([0.015])
-    # args.config["safety_layer_cfg"]["loss_action_weight"] = tune.loguniform(1e-9, 0.08)
-    args.config["safety_layer_cfg"]["loss_action_weight"] = tune.grid_search(
-        [0.1]
-        # [0.08, 7.313213261801146e-08, 0.03055703454235736]
-    )
+    args.config["safety_layer_cfg"]["loss_action_weight"] = tune.loguniform(1e-9, 0.08)
+    # args.config["safety_layer_cfg"]["loss_action_weight"] = tune.grid_search(
+    #     # [0.1]
+    #     [0.08, 7.313213261801146e-08, 0.03055703454235736]
+    # )
     args.config["safety_layer_cfg"]["batch_size"] = tune.grid_search(
         # [128, 256, 512, 1024]
         [256]
@@ -125,7 +125,7 @@ def train(args):
             "training_iteration": args.config["safety_layer_cfg"]["num_epochs"],
             "time_total_s": args.duration,
         },
-        # num_samples=18,
+        num_samples=6,
         resources_per_trial={"cpu": 1, "gpu": 0.3},
         config=args.config,
         # checkpoint_freq=5,
