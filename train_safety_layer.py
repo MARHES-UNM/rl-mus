59ed81ae219e6b1e23c9a1a8bd06072da3d6f4ee
Jean-Elie Pierre <gno320@gmail.com>
2023-10-10 21:44:04 -0400

    rad=1.0

--- train_safety_layer.py
+++ train_safety_layer.py
@@ -80,14 +80,14 @@ def train_safety_layer(config, checkpoint_dir=None):
 def train(args):
     args.config["safety_layer_cfg"]["device"] = "cuda"
     args.config["env_config"]["target_v"] = tune.grid_search([0.0])
-    args.config["env_config"]["num_obstacles"] = 8
-    args.config["env_config"]["max_num_obstacles"] = 30
+    args.config["env_config"]["num_obstacles"] = 4
+    args.config["env_config"]["max_num_obstacles"] = 4
     args.config["safety_layer_cfg"]["loss_action_weight"] = tune.grid_search(
         [0.1]
         # [1.0, 0.8, 0.5]
         # [1.0, 0.8, 0.5]
     )
-    args.config["env_config"]["obstacle_radius"] = tune.grid_search([0.1])
+    args.config["env_config"]["obstacle_radius"] = tune.grid_search([1.0])
     args.config["safety_layer_cfg"]["num_training_steps"] = 6000
     args.config["safety_layer_cfg"]["num_epochs"] = 200
     # args.config["safety_layer_cfg"]["num_iter_per_epoch"] = tune.grid_search([50])
