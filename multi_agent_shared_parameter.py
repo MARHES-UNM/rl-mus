9cbadce633e0b862d9c4076d4bc37c168af9e698
Jean-Elie Pierre <gno320@gmail.com>
2023-10-19 17:45:41 -0400

    ablation study

--- multi_agent_shared_parameter.py
+++ multi_agent_shared_parameter.py
@@ -87,16 +87,18 @@ def train(args):
     temp_env = UavSim(args.config)
     num_gpus = int(os.environ.get("RLLIB_NUM_GPUS", args.gpu))
 
-    args.config["env_config"]["use_safe_action"] = tune.grid_search([False])
-    args.config["env_config"]["beta"] = tune.grid_search([0.01, 0.1])
+    args.config["env_config"]["use_safe_action"] = tune.grid_search([False, True])
+    args.config["env_config"]["beta"] = tune.grid_search([0.01])
     args.config["env_config"]["dt_go_penalty"] = tune.grid_search([10])
-    args.config["env_config"]["d_thresh"] = tune.grid_search([0.2])
-    args.config["env_config"]["uav_collision_weight"] = tune.grid_search([0.1])
-    args.config["env_config"]["obstacle_collision_weight"] = tune.grid_search([0.15])
+    args.config["env_config"]["d_thresh"] = tune.grid_search([0.01, 0.2])
+    args.config["env_config"]["uav_collision_weight"] = tune.grid_search([0.1, 100])
+    args.config["env_config"]["obstacle_collision_weight"] = tune.grid_search(
+        [0.15, 10]
+    )
     args.config["env_config"]["stp_penalty"] = tune.grid_search([200])
     args.config["env_config"]["dt_reward"] = tune.grid_search([500])
     args.config["env_config"]["tgt_reward"] = tune.grid_search([100])
-    args.config["env_config"]["dt_weight"] = tune.grid_search([10.0, 0.1])
+    args.config["env_config"]["dt_weight"] = tune.grid_search([0.1, 0.5])
 
     entropy_coef = tune.grid_search([0.00])
 
