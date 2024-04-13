e88d8d0cbef295e6b3ce1056964e552dcd711fff
Jean-Elie Pierre <gno320@gmail.com>
2024-04-13 12:52:35 -0400

    safe rendezvous

--- train_uavs_no_cur.sh
+++ train_uavs_no_cur.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python run_experiment.py --name raw_tg_go --run PPO \
+python run_experiment.py --name safe_ren --run PPO \
     train \
     --cpu 8 \
     --gpu 0.5 \
-    --stop_timesteps 20000000
+    --stop_timesteps 30000000
