e0fcc46a4e95c3788be6ed2c4e5f3c13bf6d5f9a
Jean-Elie Pierre <gno320@gmail.com>
2024-04-18 06:03:11 -0400

    rand stp

--- train_uavs_no_cur.sh
+++ train_uavs_no_cur.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python run_experiment.py --name dt_go_fix_rew_long --run PPO \
+python run_experiment.py --name rand_stp --run PPO \
     train \
     --cpu 8 \
     --gpu 0.5 \
-    --stop_timesteps 30000000
+    --stop_timesteps 15000000
