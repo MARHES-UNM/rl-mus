fa0aa8d0678eccb4f72f249dbb505ada0c0a3aae
Jean-Elie Pierre <gno320@gmail.com>
2024-04-20 16:35:04 -0400

    stp sum high

--- run_experiment.py
+++ run_experiment.py
@@ -179,7 +179,7 @@ def train(args):
     args.config["env_config"]["stp_penalty"] = tune.grid_search(
         [10, 15, 3, 5]
     )
-    args.config["env_config"]["t_go_error_func"] = tune.grid_search(["mean"])
+    args.config["env_config"]["t_go_error_func"] = tune.grid_search(["sum"])
     args.config["env_config"]["max_dt_std"] = tune.grid_search([0.25])
     args.config["env_config"]["tgt_reward"] = 50
     args.config["env_config"]["sa_reward"] = 50
--- train_uavs_no_cur.sh
+++ train_uavs_no_cur.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python run_experiment.py --name stp_pen_mean_high --run PPO \
+python run_experiment.py --name stp_pen_sum_high --run PPO \
     train \
     --cpu 8 \
     --gpu 0.5 \
