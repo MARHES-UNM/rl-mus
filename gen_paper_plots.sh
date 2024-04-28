df2ae32403f5d979c2e2ba200bac24317daebc16
Jean-Elie Pierre <gno320@gmail.com>
2024-04-28 00:05:15 -0400

    added script to make plots

---
+++ gen_paper_plots.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python plot_results_dirty.py --exp_config configs/exp_ren_paper_no_vl_concensus_comp.json \
+    --exp_folder results/test_results/ppo_ren_comp_no_vl/exp_2024-04-27-20-44_9fab435_vl_0 \
+    --img_folder no_vl_concensus_comp \
+    --skip_legend --plot_type bar
+
+python plot_results_dirty.py --exp_config configs/exp_ren_paper_no_vl_ppo_ren_comp.json \
+    --exp_folder results/test_results/ppo_ren_comp_no_vl/exp_2024-04-27-20-44_9fab435_vl_0 \
+    --img_folder no_vl_ren_comparison \
+    --skip_legend --plot_type bar
