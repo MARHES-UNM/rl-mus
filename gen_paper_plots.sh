45ca611d7a8377212a9d437efd7e0e84fb64a3f1
Jean-Elie Pierre <gno320@gmail.com>
2024-04-28 22:58:55 -0400

    made configs to generate paper

--- gen_paper_plots.sh
+++ gen_paper_plots.sh
@@ -9,3 +9,8 @@ python plot_results_dirty.py --exp_config configs/exp_ren_paper_no_vl_ppo_ren_co
     --exp_folder results/test_results/ppo_ren_comp_no_vl/exp_2024-04-27-20-44_9fab435_vl_0 \
     --img_folder no_vl_ren_comparison \
     --skip_legend --plot_type bar
+
+python plot_results_dirty.py --exp_config configs/exp_ren_paper_vl_ppo_ttc_comp.json \
+    --exp_folder results/test_results/ppo_ren_comp_vl/exp_2024-04-28-00-50_df2ae32_vl_1 \
+    --img_folder vl_ren_comparison \
+    --skip_legend --plot_type bar
