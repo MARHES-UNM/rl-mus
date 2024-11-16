839dbf729b6724d92c815e0b6ca069bd3355fd90
Jean-Elie Pierre <gno320@gmail.com>
2024-11-16 13:09:54 -0500

    fix plot_train_results.py

--- plot_train_results.py
+++ plot_train_results.py
@@ -1,8 +1,8 @@
 # %% [markdown]
-# ## Visualizing result from tensorboard. 
+# ## Visualizing result from tensorboard.
 # #### from tutorial: https://www.tensorflow.org/tensorboard/dataframe_api
-# 
-# #### The following packages are required:  
+#
+# #### The following packages are required:
 # ```
 # pip install tensorboard pandas
 # pip install matplotlib seaborn
@@ -11,6 +11,7 @@
 # %%
 import pandas as pd
 from matplotlib import pyplot as plt
+
 plt.style.use(r"configs/paper_plot_style.mplstyle")
 # plt.style.use("paper_plot_style.mplstyle")
 import seaborn as sns
@@ -24,6 +25,7 @@ import pathlib
 
 # %% [markdown]
 # ## Helper functions
+
 
 # %%
 def parse_key(reg_exp, str_to_parse):
@@ -39,14 +41,14 @@ def parse_key(reg_exp, str_to_parse):
     found = re.findall(reg_exp, str_to_parse)
     result = "seed_n/a"
     if found:
-        result = found[0].replace('=', '_')
+        result = found[0].replace("=", "_")
     print(f"found: {result}")
     return result
-    
-    
+
+
 # %%
-# Get 
-def get_progress(basedir, string_pattern, filter_list):
+# Get
+def get_progress(basedir, experiment_name, filter_list):
     """Give a base directory, the get progress.csv file and parse it
 
     Args:
@@ -62,32 +64,29 @@ def get_progress(basedir, string_pattern, filter_list):
 
     df_list = []
     df_keys = []
-    pattern = re.compile(string_pattern)
 
     for progress in all_progress:
         print(progress.absolute())
         # filter_list = ["policy_reward_mean/pursuer", "training_iteration"]
 
         df_list.append(pd.read_csv(str(progress.absolute())).filter(items=filter_list))
-        df_keys.append(parse_key(pattern, str(progress.absolute())))
+        df_keys.append(experiment_name)
 
     df = pd.concat(df_list, keys=df_keys, names=["experiment_name"]).reset_index()
 
     return df
 
+
 # %% [markdown]
 # ### Constants
-# 
+#
 
 # %%
 # CONSTANTS
-image_output_folder = (
-    r"paper_figs/train_results"
-)
+image_output_folder = r"paper_figs/train_results"
 
 if not os.path.exists(image_output_folder):
     os.makedirs(image_output_folder, exist_ok=True)
-
 
 
 filter_list = [
@@ -101,23 +100,23 @@ filter_list = [
     "custom_metrics/num_uav_landed_mean",
     "custom_metrics/uav_done_dt_mean",
     "custom_metrics/uav_rel_dist_mean",
-    "policy_reward_mean/shared_policy"
+    "policy_reward_mean/shared_policy",
 ]
 labels_to_plot = [
-    'PPO',
-    'PPO_CUR',
+    "PPO",
+    "PPO_CUR",
 ]
 
 # %%
-basedir = r'/home/prime/Documents/workspace/rl_multi_uav_sim/ray_results/train/PPO/multi-uav-ren-v0_2024-04-27-08-59_47fb018_4u_4o/replicate_dc8b9b2_02_05/replicate_dc8b9b2_02_05/PPO_multi-uav-ren-v0_0cf50_00000_0_early_done=False,max_dt_go_error=0.2000,max_dt_std=0.0500,stp_penalty=1.5000,t_go_error_func=su_2024-04-27_08-59-58'
-df_ppo_no_cur= get_progress(basedir, r"stp_penalty=.*\d+", filter_list)
-df_ppo_no_cur.head
+basedir = r"checkpoints/uav_goal"
+df_ppo_no_cur = get_progress(basedir, "uav_goal", filter_list)
+print(df_ppo_no_cur.head)
 
 
 # %%
-basedir = r'/home/prime/Documents/workspace/rl_multi_uav_sim/ray_results/train/PPO/multi-uav-ren-v0_2024-04-27-08-59_47fb018_4u_4o/replicate_dc8b9b2_02_05/replicate_dc8b9b2_02_05/PPO_multi-uav-ren-v0_0cf50_00002_2_early_done=False,max_dt_go_error=0.2000,max_dt_std=0.0500,stp_penalty=0.0000,t_go_error_func=su_2024-04-27_08-59-58'
-df_ppo_cur= get_progress(basedir, r"stp_penalty=.*\d+", filter_list)
-df_ppo_cur.head
+basedir = r"checkpoints/uav_same_time"
+df_ppo_cur = get_progress(basedir, "uav_same_time", filter_list)
+print(df_ppo_cur.head)
 
 # %%
 # print keys
@@ -128,8 +127,8 @@ print(keys)
 
 # %%
 labels_keys = {
-    'POETIC': 'stp_penalty_1.5000,t_go_error_func_su_2024-04-27_08-59-58',
-    'POETIC_NO_REN': 'stp_penalty_0.0000,t_go_error_func_su_2024-04-27_08-59-58',
+    "POETIC": "uav_same_time",
+    "POETIC_NO_REN": "uav_goal",
 }
 
 # %%
@@ -144,21 +143,29 @@ labels_keys = {
 # custom_metrics/uav_rel_dist_mean
 # policy_reward_mean/shared_policy
 parameter_keys = {
-               "Mean Reward ": "policy_reward_mean/shared_policy",
-               "Mean UAV Dest": "custom_metrics/num_uav_landed_mean",
-               "Mean UAV Collisions": "custom_metrics/uav_collisions_mean",
-               "Mean NCFO Collisions": "custom_metrics/obstacle_collisions_mean",
-               "Mean UAV $\Delta t$": "custom_metrics/uav_done_dt_mean",
-               "Mean UAV $\Delta t std$": "custom_metrics/uav_done_time_std_mean"
-
+    "Mean Reward ": "policy_reward_mean/shared_policy",
+    "Mean UAV Dest": "custom_metrics/num_uav_landed_mean",
+    "Mean UAV Collisions": "custom_metrics/uav_collisions_mean",
+    "Mean NCFO Collisions": "custom_metrics/obstacle_collisions_mean",
+    "Mean UAV $\Delta t$": "custom_metrics/uav_done_dt_mean",
+    "Mean UAV $\Delta t std$": "custom_metrics/uav_done_time_std_mean",
 }
 
 # %%
 
 # sns.color_palette("colorblind")
 
-        
-def plot_parameters(df_groups, labels_to_plot, parameter_keys, labels_keys, fig_name, window_size=5, x_variable='training_iteration', x_label='Training Iteration'):
+
+def plot_parameters(
+    df_groups,
+    labels_to_plot,
+    parameter_keys,
+    labels_keys,
+    fig_name,
+    window_size=5,
+    x_variable="training_iteration",
+    x_label="Training Iteration",
+):
     for parameter_key, parameter in parameter_keys.items():
 
         # fig, ax = plt.subplots(figsize=(12, 10))
@@ -173,30 +180,49 @@ def plot_parameters(df_groups, labels_to_plot, parameter_keys, labels_keys, fig_
             serie_to_plot = df_groups.get_group(key)
             x_var = serie_to_plot[x_variable].to_numpy()
             variable = serie_to_plot[parameter]
-            
-            running_mean = variable.rolling(window_size, min_periods=1).mean().to_numpy()
+
+            running_mean = (
+                variable.rolling(window_size, min_periods=1).mean().to_numpy()
+            )
             running_std = variable.rolling(window_size, min_periods=1).std().to_numpy()
-            ax.fill_between(x_var, running_mean+running_std, running_mean-running_std, 
-                        alpha=0.25)
+            ax.fill_between(
+                x_var,
+                running_mean + running_std,
+                running_mean - running_std,
+                alpha=0.25,
+            )
             # sns.lineplot(x=x_var, y=running_mean)
             ax.plot(x_var, running_mean, label=label)
             # ax.grid()
             # ax.legend()
             ax.set_xlabel(x_label)
             ax.set_ylabel(parameter_key)
-            
-            fig.savefig(os.path.join(image_output_folder,f"{fig_name}_{parameter.replace('/', '_')}.png")) 
+
+            fig.savefig(
+                os.path.join(
+                    image_output_folder, f"{fig_name}_{parameter.replace('/', '_')}.png"
+                )
+            )
 
     figsize = (18, 3)
     fig_leg = plt.figure(figsize=figsize)
     ax_leg = fig_leg.add_subplot(111)
     # add the legend from the previous axes
-    ax_leg.legend(*ax.get_legend_handles_labels(), loc='center', ncol=3)
+    ax_leg.legend(*ax.get_legend_handles_labels(), loc="center", ncol=3)
     # hide the axes frame and the x/y labels
-    ax_leg.axis('off')
-    fig_leg.savefig(os.path.join(image_output_folder, 'ppo_train_legend.png'))   
-      
-plot_parameters(df_groups, labels_to_plot, parameter_keys, labels_keys, x_variable="timesteps_total", x_label="Training Time Step", fig_name="ppo_vs_ppo_no_cur")
+    ax_leg.axis("off")
+    fig_leg.savefig(os.path.join(image_output_folder, "ppo_train_legend.png"))
+
+
+plot_parameters(
+    df_groups,
+    labels_to_plot,
+    parameter_keys,
+    labels_keys,
+    x_variable="timesteps_total",
+    x_label="Training Time Step",
+    fig_name="ppo_vs_ppo_no_cur",
+)
 
 # %%
 # Get the total time it takes to train the models
@@ -204,12 +230,11 @@ for label, key in labels_keys.items():
     # get the serie to plot
     serie_to_plot = df_groups.get_group(key)
     print(f"model_name: {label}")
-    time_total_s = serie_to_plot['time_total_s'].max()
-    m, s = divmod(time_total_s,  60)
+    time_total_s = serie_to_plot["time_total_s"].max()
+    m, s = divmod(time_total_s, 60)
     h, m = divmod(m, 60)
     # time_total_h = time_total_s / 3600
     print(f"total time (s): {serie_to_plot['time_total_s'].max()}")
-    print(f'h:m:s: \t{h:.0f}:{m:.0f}:{s:.2f}')
+    print(f"h:m:s: \t{h:.0f}:{m:.0f}:{s:.2f}")
     # print(f"total time (m): {time_total_m}")
     # print(f"total time (h): {time_total_h}")
-
