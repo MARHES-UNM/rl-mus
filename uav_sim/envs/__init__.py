94900eb838abe3f0472e4998118183eb372124f0
Jean-Elie Pierre <gno320@gmail.com>
2025-04-05 15:16:34 -0400

    training with curriculum learning

--- uav_sim/envs/__init__.py
+++ uav_sim/envs/__init__.py
@@ -2,6 +2,7 @@ from ray import tune
 from uav_sim.envs.uav_sim import UavSim
 from uav_sim.envs.uav_rl_ren import UavRlRen
 from uav_sim.envs.curriculum_uav_sim import CurriculumEnv
+from uav_sim.envs.curriculum_uav_rl_ren import CurriculumUavRlRen
 
 tune.register_env(
     "multi-uav-sim-v0",
@@ -17,3 +18,9 @@ tune.register_env(
     "multi-uav-sim-curriculum-v0",
     lambda env_config: CurriculumEnv(config=env_config),
 )
+
+tune.register_env(
+    "multi-uav-ren-curriculum-v0",
+    lambda env_config: CurriculumUavRlRen(config=env_config),
+)
+
